351f70ccb8f713fc6f1d9af0751c20052bd74bc5
Randall Sewell <randallsewell@fastmail.com>
2024-12-24 18:55:38 +0000

    Fix space in the build script

--- scripts/rebuild-prod.sh
+++ scripts/rebuild-prod.sh
@@ -9,7 +9,7 @@ echo "Stopping existing services..."
 docker compose -f ./docker/docker-compose.yml -f ./docker/docker-compose.prod.yml --env-file .env down
 
 echo "Rebuilding images..."
-docker compose -f ./docker/docker-compose.yml -f ./docker/docker-compose.prod.yml --env-file .envbuild --no-cache
+docker compose -f ./docker/docker-compose.yml -f ./docker/docker-compose.prod.yml --env-file .env build --no-cache
 
 echo "Starting services..."
 docker compose -f ./docker/docker-compose.yml -f ./docker/docker-compose.prod.yml --env-file .env up -d
